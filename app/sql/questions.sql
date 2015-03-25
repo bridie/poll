dea1a2a28a07544afc69467ffcf91c1e60d4dee0
Bridie <bridie.dibble@gmail.com>
2015-03-25 20:24:38 +0000

    Adding the url_component as a unique key to the questions table

--- app/sql/questions.sql
+++ app/sql/questions.sql
@@ -2,5 +2,6 @@ create TABLE IF NOT EXISTS questions (
 	id int(11) NOT NULL AUTO_INCREMENT,
 	question_value varchar(255) DEFAULT '',
 	url_component varchar(255) NOT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	UNIQUE KEY (url_component)
 ) ENGINE=InnoDB
